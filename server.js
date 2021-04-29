a534bc60f1588f77b507e2c5ff0c3f77b263a97d
enes <enes.3.ergun@gmail.com>
2021-04-29 04:33:06 +0200

    main plant page uncomplete

--- server.js
+++ server.js
@@ -7,21 +7,24 @@ const plants = [
     {
         id: 1,
         name: 'Orchid',
-        url: '#',
+        url:
+            'https://www.gardeningknowhow.com/wp-content/uploads/2014/01/vanda-orchid.jpg',
         watering: 'once a week',
         fertilization: 'once a month',
     },
     {
         id: 2,
         name: 'Tulip',
-        url: '#',
+        url:
+            'https://www.photos-public-domain.com/wp-content/uploads/2012/04/pink-tulip-with-one-petal-open.jpg',
         watering: 'twice a week',
         fertilization: 'twice a month',
     },
     {
         id: 3,
         name: 'Cactus',
-        url: '#',
+        url:
+            'https://succulentcity.com/wp-content/uploads/2019/05/parodia-magnifica-cactus-sc-1024x1024.jpg',
         watering: 'once a year',
         fertilization: 'twice a year',
     },
